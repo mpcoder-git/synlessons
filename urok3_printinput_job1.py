589b63b558e067bf1d92766f905ed5d041c9d491
mpcoder-git <mpcoder@mail.ru>
2023-11-25 11:45:15 +0600

    добавление урока 3

---
+++ urok3_printinput_job1.py
@@ -0,0 +1,6 @@
+#1 job
+vid_pit = input("Введите вид питомца: ")
+vozrast = input("Введите возраст питомца: ")
+klichka = input("Введите кличку питомца: ")
+
+print (f'Это {vid_pit} по кличке {klichka} . Возраст: {vozrast} года.')
